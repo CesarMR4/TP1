c46fd9ee6184eae96a3b38a3dbba85f0e526e600
César Martinez <122939628+CesarMR4@users.noreply.github.com>
2025-04-27 18:52:51 -0500

    commit

--- TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/Tp1Nexadvisor1Application.java
+++ TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/Tp1Nexadvisor1Application.java
@@ -8,6 +8,7 @@ public class Tp1Nexadvisor1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp1Nexadvisor1Application.class, args);
+		System.out.println("Hola, mundo");
 	}
 
 }
