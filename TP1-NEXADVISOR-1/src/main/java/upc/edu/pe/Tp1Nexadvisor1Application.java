505dcec5f0fcf03008e830aea2284685f760c0e3
César Martinez <122939628+CesarMR4@users.noreply.github.com>
2025-04-27 18:19:24 -0500

    MI PRIMER COMMIT

---
+++ TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/Tp1Nexadvisor1Application.java
@@ -0,0 +1,13 @@
+package upc.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1Nexadvisor1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1Nexadvisor1Application.class, args);
+	}
+
+}
