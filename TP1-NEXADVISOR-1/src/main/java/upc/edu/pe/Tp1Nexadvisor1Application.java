a109484716ee9389f0e458aec36499478c497a3e
Mircco <mircco_hf@hotmail.com>
2025-04-27 19:15:30 -0500

    Prueba 1

--- TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/Tp1Nexadvisor1Application.java
+++ TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/Tp1Nexadvisor1Application.java
@@ -9,6 +9,7 @@ public class Tp1Nexadvisor1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Tp1Nexadvisor1Application.class, args);
 		System.out.println("Hola, mundo");
+		System.out.println("Hola, mundo 2");
 	}
 
 }
