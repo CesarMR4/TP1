0d9ac4d83a114f5328efd98cf4f37e2ee479c593
Mircco <mircco_hf@hotmail.com>
2025-04-29 18:42:46 -0500

    Asesor

---
+++ TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/entities/Asesor.java
@@ -0,0 +1,126 @@
+package upc.edu.pe.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Asesor")
+public class Asesor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id_Asesor;
+	@Column(name = "nombre", nullable = false, length = 25)
+    private String nombre;
+	@Column(name = "email", nullable = false, length = 25)
+    private String email;
+	@Column(name = "contrasena", nullable = false, length = 25)
+    private String contrasena;
+	@Column(name = "direccion", nullable = false, length = 25)
+    private String direccion;
+	@Column(name = "telefono", nullable = false, length = 25)
+    private String telefono;
+	@Column(name = "curriculum", nullable = false, length = 25)
+    private String curriculum; 
+	@Column(name = "sector", nullable = false, length = 25)
+    private String sector;
+	@Column(name = "carrera", nullable = false, length = 25)
+    private String carrera;
+	@Column(name = "fecharegistro", nullable = false, length = 25)
+    private Date fechaRegistro;
+	@Column(name = "rol", nullable = false, length = 10)
+    private String rol = "asesor";
+	public int getId_Asesor() {
+		return id_Asesor;
+	}
+	public void setId_Asesor(int id_Asesor) {
+		this.id_Asesor = id_Asesor;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCurriculum() {
+		return curriculum;
+	}
+	public void setCurriculum(String curriculum) {
+		this.curriculum = curriculum;
+	}
+	public String getSector() {
+		return sector;
+	}
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	public String getRol() {
+		return rol;
+	}
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	public Asesor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Asesor(int id_Asesor, String nombre, String email, String contrasena, String direccion, String telefono,
+			String curriculum, String sector, String carrera, Date fechaRegistro, String rol) {
+		super();
+		this.id_Asesor = id_Asesor;
+		this.nombre = nombre;
+		this.email = email;
+		this.contrasena = contrasena;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.curriculum = curriculum;
+		this.sector = sector;
+		this.carrera = carrera;
+		this.fechaRegistro = fechaRegistro;
+		this.rol = rol;
+	}
+	
+	
+}
