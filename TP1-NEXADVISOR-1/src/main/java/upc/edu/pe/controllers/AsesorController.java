928701c7f52c8673fb6dec7260cbb5a9451b3158
Mircco <mircco_hf@hotmail.com>
2025-04-30 00:06:56 -0500

    Asesor controller

--- TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/controllers/AsesorController.java
+++ TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/controllers/AsesorController.java
@@ -1,5 +1,48 @@
 package upc.edu.pe.controllers;
 
-public class AsesorController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import upc.edu.pe.entities.Asesor;
+import upc.edu.pe.serviceinterface.AsesorService;
+
+@Controller
+@RequestMapping("/asesores")
+public class AsesorController {
+	
+	@Autowired
+	private AsesorService asesorService;
+	
+	@GetMapping("/nuevo")
+	public String newAsesor(Model model) { 
+		model.addAttribute("a", new Asesor());
+		return "asesor/fmrRegistro";
+		
+	}
+	
+	@PostMapping("/guardar")
+	public String saveAsesor(Asesor ase, BindingResult binRes, Model model) {
+	
+		if (binRes.hasErrors()) {
+			return "asesor/frmRegistro";
+		}
+		else {
+			asesorService.insert(ase);
+			model.addAttribute("mensaje", "Se registro Correctamente");
+			return "redirect:/asesores/nuevo";
+		}
+	}
+	
+	
 }
+		
+		
+
+	
+
+
