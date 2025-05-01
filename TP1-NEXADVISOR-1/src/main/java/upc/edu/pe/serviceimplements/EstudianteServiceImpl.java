980aca4e40b58d14369c1a1a626b77c535b73e4d
César Martinez <122939628+CesarMR4@users.noreply.github.com>
2025-05-01 11:57:25 -0500

    Realizando el ServiceImplements de Estudiante

---
+++ TP1-NEXADVISOR-1/src/main/java/upc/edu/pe/serviceimplements/EstudianteServiceImpl.java
@@ -0,0 +1,36 @@
+package org.springframework.stereotype;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import upc.edu.pe.entities.Estudiante;
+import upc.edu.pe.repositories.EstudianteRepository;
+import upc.edu.pe.serviceinterface.EstudianteService;
+
+@Service
+public class EstudianteServiceImpl implements EstudianteService {
+	
+	@Autowired
+	private EstudianteRepository pR;
+	
+	@Override 
+	public void insert(Estudiante estudiante) {
+		pR.save(estudiante);
+	}
+	@Override
+	 public List<Estudiante> list() {
+        return pR.findAll();
+    }
+	 @Override
+	 public void delete(int idEstudiante) {pR.deleteById(idEstudiante);}
+	 
+	 @Override
+		public Optional<Estudiante> listId(int id) {
+			return pR.findById(id);
+		}
+	
+
+}
